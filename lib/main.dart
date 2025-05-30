c3b32f1ad95478a5eb1589b9b59eefa771ff174e
Mehran Haider <mehranhaider1786@gmail.com>
2025-05-30 18:44:44 +0500

    route managment feature done app is ready for first launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
+import 'package:findmybus/firebase_options.dart';
 import 'package:findmybus/screens/LoginScreen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Find My Bus',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
   }
 }
